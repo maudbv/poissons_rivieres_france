364716432477ab9ba7ab0e1d53b465543bfc4193
maudbv <maudbv@gmail.com>
2017-01-30 17:05:17 +0100

    example data

--- scripts/import data.R
+++ scripts/import data.R
@@ -1,8 +1,15 @@
 # Import data sets
+library(readxl)
 
+# fishing data
+
+## eg for 2000-2009 in Alsace:
+peches <- read_excel("data/resultats peches/Exploitation_Alsace_2010-2013.xls", 
+                      sheet = 2)
+peches$`Date de pêche` <- as.Date(peches$`Date de pêche`)
+               
+               
 # data for each operation
-
-# fish abundance data
 
 # fish size class data
 
